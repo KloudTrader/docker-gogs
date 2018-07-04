5b1b3855773c6b1071de478ef240195db486bc72
Rob Beal <rob@kohi.uk>
2018-07-04 18:29:59 +0100

    Fix build for aarch64

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ case ${arch} in
     i386 ) base_image="resin/i386-alpine" ;;
     amd64 ) base_image="alpine:3.7" ;;
     arm ) base_image="resin/rpi-alpine" ;;
-    arm64 ) base_image="resin/aarch64-alpine" ;;
+    aarch64 ) base_image="resin/aarch64-alpine" ;;
 esac
 cp Dockerfile Dockerfile.${arch}
 sed -i "s@alpine:3.7@$base_image@g" Dockerfile.${arch}
